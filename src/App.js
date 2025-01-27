54ede78326bcbdf148034140fc7fe7fe2cf341ec
omgacai <134914115+omgacai@users.noreply.github.com>
2025-01-27 16:10:20 +0800

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Home/NavBar';
 import Home from './Pages/Home/Homescreen/Index';
