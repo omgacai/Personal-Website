918529c6dfef5442097854676b36e6b4794950ef
omgacai <134914115+omgacai@users.noreply.github.com>
2025-01-27 16:21:03 +0800

    revert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Home/NavBar';
 import Home from './Pages/Home/Homescreen/Index';
