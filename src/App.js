a4ceba4c948509e7c0d09443a4cfbee98d4beff9
omgacai <134914115+omgacai@users.noreply.github.com>
2025-05-30 09:18:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Pages/Home/NavBar';
 import Home from './Pages/Home/Homescreen/Index';
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
             <Route path = "/" element = {<Home />}></Route>
             <Route path = "*" element = {<div>404 Not Found</div>}> </Route>
           </Routes>
+          <Analytics />
         </div>
       </Router>
     </div>
